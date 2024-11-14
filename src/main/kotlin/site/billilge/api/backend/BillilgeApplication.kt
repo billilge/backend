9d7ddbd5a2eb90b0aa9d630a3a49fa796347b04c
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-11-15 02:00:40 +0900

    init: 초기 세팅

---
+++ src/main/kotlin/site/billilge/api/backend/BillilgeApplication.kt
@@ -0,0 +1,11 @@
+package site.billilge.api.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BillilgeApplication
+
+fun main(args: Array<String>) {
+    runApplication<BillilgeApplication>(*args)
+}
---
+++ src/test/kotlin/site/billilge/api/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package site.billilge.api.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
