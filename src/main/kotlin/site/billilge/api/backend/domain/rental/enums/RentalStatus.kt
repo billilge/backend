343612ac338ac7616a4c07206d42bcd5bc5efe2a
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2024-12-27 16:43:33 +0900

    fix: 대여 상태 표기 오류 수정

--- src/main/kotlin/site/billilge/api/backend/domain/rental/enums/RentalStatus.kt
+++ src/main/kotlin/site/billilge/api/backend/domain/rental/enums/RentalStatus.kt
@@ -3,7 +3,8 @@ package site.billilge.api.backend.domain.rental.enums
 enum class RentalStatus(val displayName: String) {
     PENDING("승인 대기 중"),
     CANCEL("대기 취소"),
-    CONFIRMED("소모품 대여 완료"),
+    CONFIRMED("승인 완료"),
+    RENTAL_COMPLETED("소모품 대여 완료"),
     RENTAL("대여 중"),
     RETURNED("반납 완료")
 }
