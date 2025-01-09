8cfa6d362b636899984c3d85aa12bf0ab6ea2812
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-01-09 16:55:16 +0900

    refactor(#1): ITEM_NAME_ALREADY_EXISTS 에러의 응답 코드 수정

--- src/main/kotlin/site/billilge/api/backend/domain/admin/exception/AdminErrorCode.kt
+++ src/main/kotlin/site/billilge/api/backend/domain/admin/exception/AdminErrorCode.kt
@@ -7,6 +7,6 @@ enum class AdminErrorCode(
     override val message: String,
     override val httpStatus: HttpStatus
 ) : ErrorCode {
-    ITEM_NAME_ALREADY_EXISTS("이미 존재하는 물품 이름입니다.", HttpStatus.valueOf(418)),
+    ITEM_NAME_ALREADY_EXISTS("이미 존재하는 물품 이름입니다.", HttpStatus.BAD_REQUEST),
     ITEM_NOT_FOUND("물품 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
 }
