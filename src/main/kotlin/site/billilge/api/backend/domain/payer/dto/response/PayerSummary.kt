4d870977e9f6785c9b12ca7b8f92eb31d04e6513
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-02-16 14:40:35 +0900

    feat: 납부자 조회 응답에 id 추가

--- src/main/kotlin/site/billilge/api/backend/domain/payer/dto/response/PayerSummary.kt
+++ src/main/kotlin/site/billilge/api/backend/domain/payer/dto/response/PayerSummary.kt
@@ -3,6 +3,7 @@ package site.billilge.api.backend.domain.payer.dto.response
 import site.billilge.api.backend.domain.payer.entity.Payer
 
 data class PayerSummary(
+    val payerId: Long,
     val name: String,
     val studentId: String,
     val registered: Boolean
@@ -13,6 +14,7 @@ data class PayerSummary(
             val studentId = payer.studentId ?: "${payer.enrollmentYear}XXXX"
 
             return PayerSummary(
+                payerId = payer.id!!,
                 name = payer.name,
                 studentId = studentId,
                 registered = payer.registered
