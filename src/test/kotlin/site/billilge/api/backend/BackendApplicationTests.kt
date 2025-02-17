8cf3809ef85bfafcf0f8ae196fd3066b05886227
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-02-17 20:48:10 +0900

    fix: gradle build되지 않던 현상 수정

--- src/test/kotlin/site/billilge/api/backend/BackendApplicationTests.kt
+++ src/test/kotlin/site/billilge/api/backend/BackendApplicationTests.kt
@@ -2,7 +2,9 @@ package site.billilge.api.backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("local")
 @SpringBootTest
 class BackendApplicationTests {
 
