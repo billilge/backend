34b19e429f43c95e55b9f0ffd402b1466d25cb86
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-02-18 21:58:57 +0900

    refactor(#39): RentalHistoryDetail에 Id 값 Summary로 대체

--- src/main/kotlin/site/billilge/api/backend/domain/rental/dto/response/RentalHistoryDetail.kt
+++ src/main/kotlin/site/billilge/api/backend/domain/rental/dto/response/RentalHistoryDetail.kt
@@ -1,13 +1,15 @@
 package site.billilge.api.backend.domain.rental.dto.response
 
+import site.billilge.api.backend.domain.item.dto.response.ItemSummary
+import site.billilge.api.backend.domain.member.dto.response.MemberSummary
 import site.billilge.api.backend.domain.rental.entity.RentalHistory
 import site.billilge.api.backend.domain.rental.enums.RentalStatus
 import java.time.LocalDateTime
 
 data class RentalHistoryDetail(
     val rentalHistoryId: Long,
-    val memberId: Long,
-    val itemId: Long,
+    val memberId: MemberSummary,
+    val itemId: ItemSummary,
     val rentAt: LocalDateTime,
     val returnedAt: LocalDateTime?,
     val rentalStatus: RentalStatus
@@ -17,8 +19,8 @@ data class RentalHistoryDetail(
         fun from(rentalHistory: RentalHistory): RentalHistoryDetail {
             return RentalHistoryDetail(
                 rentalHistoryId = rentalHistory.id!!,
-                memberId = rentalHistory.member.id!!,
-                itemId = rentalHistory.item.id!!,
+                memberId = MemberSummary.from(rentalHistory.member),
+                itemId = ItemSummary.from(rentalHistory.item),
                 rentAt = rentalHistory.rentAt,
                 returnedAt = rentalHistory.returnedAt,
                 rentalStatus = rentalHistory.rentalStatus
