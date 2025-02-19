36c61647457b643f17c9a04d3a4304985216ac01
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-02-19 19:50:52 +0900

    refactor(#43): schema 어노테이션에 field 추가

--- src/main/kotlin/site/billilge/api/backend/domain/rental/dto/request/RentalHistoryRequest.kt
+++ src/main/kotlin/site/billilge/api/backend/domain/rental/dto/request/RentalHistoryRequest.kt
@@ -4,24 +4,24 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "대여 기록 생성 요청 DTO")
 data class RentalHistoryRequest(
-    @Schema(description = "대여할 물품의 ID", example = "1")
+    @field:Schema(description = "대여할 물품의 ID", example = "1")
     val itemId: Long,
 
-    @Schema(description = "대여할 물품의 수량", example = "1")
+    @field:Schema(description = "대여할 물품의 수량", example = "1")
     val count: Int,
 
-    @Schema(description = "대여 시작 시간 정보")
+    @field:Schema(description = "대여 시작 시간 정보")
     val rentalTime: RentalTime,
 
-    @Schema(description = "중복 대여 무시 여부 (true인 경우 중복 대여 진행)", example = "false")
+    @field:Schema(description = "중복 대여 무시 여부 (true인 경우 중복 대여 진행)", example = "false")
     val ignoreDuplicate: Boolean
 ) {
     @Schema(description = "대여 시작 시간 (시간 및 분)")
     data class RentalTime(
-        @Schema(description = "대여 시작 시각의 시간(24시간 기준)", example = "13")
+        @field:Schema(description = "대여 시작 시각의 시간(24시간 기준)", example = "13")
         val hour: Int,
 
-        @Schema(description = "대여 시작 시각의 분", example = "0")
+        @field:Schema(description = "대여 시작 시각의 분", example = "0")
         val minute: Int
     )
 }
