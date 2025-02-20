12932443afa745b7cb7aa16902cd568b81ba3018
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-02-20 16:21:19 +0900

    HOTFIX: swagger 작동되지 않던 현상 수정

--- src/main/kotlin/site/billilge/api/backend/global/config/SwaggerConfig.kt
+++ src/main/kotlin/site/billilge/api/backend/global/config/SwaggerConfig.kt
@@ -6,12 +6,23 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-@OpenAPIDefinition(info = Info(title = "빌릴게(Billilge) API", description = "국민대학교 소프트웨어융합대학 복지물품 대여 서비스", version = "v1"))
+@OpenAPIDefinition(
+    info = Info(
+        title = "빌릴게(Billilge) API",
+        description = "국민대학교 소프트웨어융합대학 복지물품 대여 서비스",
+        version = "v1"
+    )
+)
 @Configuration
-class SwaggerConfig {
+class SwaggerConfig(
+    @Value("\${swagger.server.base-url}")
+    private val serverBaseUrl: String,
+) {
     @Bean
     fun openAPI(): OpenAPI {
         val securityRequirement = SecurityRequirement().addList("JWT")
@@ -22,9 +33,14 @@ class SwaggerConfig {
                 .scheme("bearer")
                 .bearerFormat("JWT")
         )
+
+        val server = Server()
+            .apply { url = serverBaseUrl }
+
         return OpenAPI()
             .components(Components())
             .addSecurityItem(securityRequirement)
+            .servers(listOf(server))
             .components(components)
     }
 }
