4e3d4dbaadecadc42389c14cb93b6b250f4a98d6
ukly <dlwjddnr5438@kookmin.ac.kr>
2025-02-21 02:12:28 +0900

    refactor: 에러코드 메시지 오타 수정

--- src/main/kotlin/site/billilge/api/backend/domain/notification/exception/NotificationErrorCode.kt
+++ src/main/kotlin/site/billilge/api/backend/domain/notification/exception/NotificationErrorCode.kt
@@ -7,6 +7,6 @@ enum class NotificationErrorCode(
     override val message: String,
     override val httpStatus: HttpStatus
 ) : ErrorCode {
-    NOTIFICATION_NOT_FOUND("알림 정보를 찾을 없습니다.", HttpStatus.NOT_FOUND),
+    NOTIFICATION_NOT_FOUND("알림 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     NOTIFICATION_ACCESS_DENIED("알림을 수정할 권한이 없습니다.", HttpStatus.FORBIDDEN)
 }
