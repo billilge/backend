5d3516f2d7fc54ffe15baacd873d5affe81a8369
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-04-14 17:18:40 +0900

    HOTFIX: 액세스 토큰 만료 시 에러 분기 처리

--- src/main/kotlin/site/billilge/api/backend/global/exception/GlobalErrorCode.kt
+++ src/main/kotlin/site/billilge/api/backend/global/exception/GlobalErrorCode.kt
@@ -13,4 +13,5 @@ enum class GlobalErrorCode(
     IMAGE_NOT_FOUND("이미지 파일을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     IMAGE_DELETE_FAILED("이미지 삭제에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_OAUTH2_PROVIDER("유효하지 않은 OAuth2 로그인 인증기관입니다.", HttpStatus.BAD_REQUEST),
+    EXPIRED_ACCESS_TOKEN("만료된 액세스 토큰입니다.", HttpStatus.UNAUTHORIZED),
 }
