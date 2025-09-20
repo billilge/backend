1a9d2b3746d3afd39cfd443d4798fc2f4b0df791
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2025-09-20 18:47:05 +0900

    chore: 타임존 삭제

--- src/main/kotlin/site/billilge/api/backend/BillilgeApplication.kt
+++ src/main/kotlin/site/billilge/api/backend/BillilgeApplication.kt
@@ -4,13 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import java.util.*
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class BillilgeApplication
 
 fun main(args: Array<String>) {
-    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     runApplication<BillilgeApplication>(*args)
 }
